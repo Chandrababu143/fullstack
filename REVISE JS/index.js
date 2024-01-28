898222123fa77c1a54332806f20b42d1822c8788
Chandrababu143 <141106049+Chandrababu143@users.noreply.github.com>
2024-01-28 21:13:57 +0530

    Add files via upload

---
+++ REVISE JS/index.js
@@ -0,0 +1,71 @@
+// let name = "babu";
+// let mes ="haii i am " + name + " welcome ";
+
+// alert(mes);
+// console.log(mes);
+
+// let num1 =71;
+// let num2 =72;
+//  let babu =  num1 + num2 ;
+//  alert("welcome to love ");
+//  console.log(babu);
+
+// const name = "chandrababu";
+// const dob = "22/08/2001";
+// const role = "full stack developer";
+// const a ="haii";
+//  const babu = "haii i am " + name + " and my date of birth is " + dob + " and my role is  " + role   ;
+// alert(a);
+//  console.log(babu);
+
+
+//  const name = "chandrababu"
+//  const dob = "22-08-2001"
+//  let role = "full stack"
+//  role = "web development"
+//  let from ="arungurukkai"
+//  let district = "villlupuam"
+//  const address ="1/146 west street"
+//  let log = "my name is" + name + " and date of birth is " + dob + " and my role is " + role + " and my village is " + from + " and my district is " + district + " finnaly my address is " + address ;
+//  let a ="my details"
+// alert(a);
+//  console.log(log);
+//  console.log(dob);
+//  console.log(role);
+//  console.log(from);
+//  console.log(district);
+//  console.log(address);
+
+// let loc = "coimbatore";
+// let job = "developer";
+//  let res = (`you will be a ${job}   in  ${loc}`) ;
+
+//  console.log(res);
+
+// var fstName = "ajith"
+// var scdName = "kumar"
+// var age = 22
+
+// console.log(fstName,scdName,age)
+
+// var birthyear="2001";
+// var futureyear="2025";
+// var currentyear="2023";
+// var futureyear=futureyear-birthyear;
+// var currentyear=currentyear-birthyear;
+// console.log("birthyear:"+birthyear);
+// console.log("currentyear:"+currentyear);
+// console.log("futureyear:"+futureyear);
+// console.log("I Will be either"+currentyear+"and"+futureyear+"in the year"+futureyear);
+
+
+// var bd = "2001";
+// var cy = "2024"
+// var fy = "2050";
+//  var bd = fy - bd ;
+//  var cy = cy - bd ;
+
+//  console.log("bd:"+bd);
+//  console.log("cy:"+cy);
+//  console.log("fy:"+fy);
+//  console.log(" i will be either " + cy + " and " + fy + " in the year " + bd);
